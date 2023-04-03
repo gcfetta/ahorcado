e0fb2219f901d42a57e44df27ef91571700f82fa
Carolina <gcfetta@gmail.com>
2023-04-03 19:19:12 -0300

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,17 @@
 const palabrAdivinar = ingresarPalabra();
 let arrCoincidencias = [];
+let errados = 0;
 
 const letra = document.querySelector('input');  
 letra.oninput = function(){  //oninput cada vez que el ususario escriba en el input
-    soloLetras(letra.value, palabrAdivinar);
+    soloLetras(letra.value.toUpperCase(), palabrAdivinar); //convierto las letras a mayuscula tmb
 };
 
 function ingresarPalabra(){
     const palabra = prompt("Ingresa una palabra para adivinar!");
-    const arrPalabra = palabra.split("");
-    console.log(arrPalabra);
+    const uppercase = palabra.toUpperCase();
+    const arrPalabra = uppercase.split("");
+    //console.log(arrPalabra);
     document.getElementById("tablero").innerHTML = `
         <table border="1">
             <tr>
@@ -23,7 +25,7 @@ function ingresarPalabra(){
 function creaTablero(arrPalabra){
     let tablero = "";
     arrPalabra.forEach(letra => {
-        tablero = tablero + "<td> ? </td>";
+        tablero += "<td> ? </td>";
     });
     return tablero;
 };
@@ -33,7 +35,7 @@ function soloLetras(cadena, palabrAdivinar){
     console.log(pattern.test(cadena));
     if(!pattern.test(cadena)){
         document.querySelector('input').value = ""; //vuelve a nada el valor del input
-        document.getElementById("status").innerHTML = "Solo puedes ingresar letras!!!..";
+        document.getElementById("status").innerHTML = "Solo puedes ingresar letras!!!";
         return false;
     }else{
         document.getElementById("tablero").innerHTML = `
@@ -50,7 +52,8 @@ function soloLetras(cadena, palabrAdivinar){
 function buscarCoincidencia(letra, arrPalabra){
     let tablero = "";
     let coincidencias = 0;
-    arrPalabra.forEach(caracter => {
+    arrPalabra.forEach(caracter => {            
+        //console.log(caracter + letra);
         if(caracter == letra){
             tablero = tablero + "<td>"+ caracter +" </td>"; //imprime la tabla con la letra que si 
             coincidencias = coincidencias + 1;
@@ -69,3 +72,49 @@ function leyendaCoincidencia(coincidencias){
         document.getElementById("status").innerHTML = `No hubo coinciencias :(`;
     }
 };
+
+function ahorcado(errados){
+    switch(errados){
+        case 1:
+            document.getElementById("ahorcado").innerHTML = `
+            <img src="img/1.png">
+        `;
+        break;
+
+        case 2:
+            document.getElementById("ahorcado").innerHTML = `
+            <img src="img/2.png">
+        `;
+        break;
+
+        case 3:
+            document.getElementById("ahorcado").innerHTML = `
+            <img src="img/3.png">
+        `;
+        break;
+
+        case 4:
+            document.getElementById("ahorcado").innerHTML = `
+            <img src="img/4.png">
+        `;
+        break;
+
+        case 5:
+            document.getElementById("ahorcado").innerHTML = `
+            <img src="img/5.png">
+        `;
+        break;
+
+        case 6:
+            document.getElementById("ahorcado").innerHTML = `
+            <img src="img/6.png">
+        `;
+        break;
+
+        default:
+            document.getElementById("ahorcado").innerHTML = `
+            <img src="img/0.png">
+        `;
+        break;
+    }
+}
